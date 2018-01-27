1e0681048b012bce0b0a9a736318bbe34be2c83d
Stefan Seibert <stefan.seibert@autodesk.com>
2018-01-27 18:49:36 -0500

    Cleanup

--- python/export_network.py
+++ python/export_network.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 from nnao_network import build_nnao_network
 from helper.save_data import freeze_graph
 
-timecode = 1516840137
-
 def export_frozen_graph(timecode, session):
     interactive_module = tf.load_op_library("C:/code/tensorflow/tensorflow/contrib/cmake/build/Release/interactive_ops.dll")
 
